f72035aa179fb2d80f2140bf80eab3e57971002a
Daniel Song <danielsong05@g.ucla.edu>
2025-05-27 12:23:09 -0700

    Remove analyze_conversations.py and test.py from version control

--- test.py
+++
@@ -1,5 +0,0 @@
-from model_factory import load_model
-
-model = load_model("llama-13b")
-response = model.query("What are the risks of multi-turn adversarial attacks in dialogue systems?")
-print(response)
